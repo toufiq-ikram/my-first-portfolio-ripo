4c17b3f9b232bb5c6a57e13aa421fd29525fea8f
Ikram456 <mdtoufiqurrahmanikram@gmail.com>
2025-06-19 15:13:57 +0600

    added my second porfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Learner",
+    "Blogger",
+    "Traveller",
+    "Web Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
